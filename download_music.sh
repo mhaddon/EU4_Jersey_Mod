2c1c4aa906e00cacf991e7b01bd6c0210bc23e74
Michael Haddon <dis_connected@ymail.com>
2017-09-20 00:49:56 +0200

    fixed audio bugs

--- download_music.sh
+++ download_music.sh
@@ -7,9 +7,14 @@
 # https://github.com/rg3/youtube-dl/blob/master/README.md#readme
 
 echo "Downloading music from youtube, this might take a while..."
-youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_anthem.ogg" https://www.youtube.com/watch?v=pGUaM_GiQKQ &
-youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_grouville.ogg" https://www.youtube.com/watch?v=f5xQ_hEmCVM &
-youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_broadbean.ogg" https://www.youtube.com/watch?v=oqhZp0ayvs8 &
-youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_beancrock.ogg" https://www.youtube.com/watch?v=lBRPweNfzlw &
-youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_crapaud.ogg" https://www.youtube.com/watch?v=fXQdDmD9AcA
+rm -rf music/jer_anthem.ogg
+rm -rf music/jer_grouville.ogg
+rm -rf music/jer_broadbean.ogg
+rm -rf music/jer_beancrock.ogg
+rm -rf music/jer_crapaud.ogg
+youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_anthem.%(ext)s" pGUaM_GiQKQ &
+youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_grouville.%(ext)s" f5xQ_hEmCVM &
+youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_broadbean.%(ext)s" oqhZp0ayvs8 &
+youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_beancrock.%(ext)s" lBRPweNfzlw &
+youtube-dl --extract-audio --audio-format "vorbis" --output "music/jer_crapaud.%(ext)s" fXQdDmD9AcA
 
